420304b2b13125048457a61abebc468ced6eba1e
SKP <sree.puppala@gmail.com>
2022-05-26 20:30:09 -0500

    nftsophomoretrack

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xdbB9b74B3E87039365dA5048c0a7da13bbB4444C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
